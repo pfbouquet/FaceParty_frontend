8e96574582c6ee4c6a023954161cce9e5bbcec0c
Aymeric <aymeric.simao@gmail.com>
2025-05-21 12:19:43 +0200

    ajout socket sur screen home

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,6 +1,26 @@
 import { StyleSheet, Text, View, Button } from "react-native";
+import { useContext } from "react";
+import { SocketContext } from "../contexts/SocketContext";
 
 export default function Home({ navigation }) {
+	// const { socket } = useContext(SocketContext);
+
+	// Function to create one question in socket (for testing)
+	function clickQuestion(){
+		socket.emit("game-cycle", {
+			type:"question", 
+			payload:{questionID:"123456789", 
+			imageURL:"https://res.cloudinary.com/dat8yzztd/image/upload/v1747390409/soli7celoyncta2u4c4g.jpg", 
+			goodAnswer:["Toto", "Titi"], 
+			possibleAnswers:[["Toto", "Marc", "José"],["Titi", "Jean-Claude", "Chuck"]],
+			index:2,
+			askedAtTime:Date.now(),
+			answerHistory:[{playerID:"P1", answer:}]}})
+		navigation.navigate("Question")
+		// Handle question click
+		console.log("Question clicked");
+	}
+
 	return (
 		<View style={styles.container}>
 			<Text style={styles.title}>HOME (créer une partie --ADMIN-- ou rejoindre une partie -- joueur</Text>
@@ -11,7 +31,7 @@ export default function Home({ navigation }) {
 			<Button title="Go to HomeTuto" onPress={() => navigation.navigate("HomeTuto")} />
 			<Button title="Go to PlayerName" onPress={() => navigation.navigate("PlayerName")} />
 			<Button title="Go to TabNavigator" onPress={() => navigation.navigate("TabNavigator")} />
-			<Button title="Go to QUESTION" onPress={() => navigation.navigate("Question")} />
+			<Button title="Go to QUESTION" onPress={() => clickQuestion()} />
 		</View>
 	);
 }

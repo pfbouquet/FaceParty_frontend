2ff2b4fd1e5ede9e9a60bfccecf64608cefcf53e
pfbouquet <pfbouquet@gmail.com>
2025-05-21 14:04:52 +0200

    fix and env variable management

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -3,50 +3,61 @@ import { useContext } from "react";
 import { SocketContext } from "../contexts/SocketContext";
 
 export default function Home({ navigation }) {
-	// const { socket } = useContext(SocketContext);
+  // const { socket } = useContext(SocketContext);
 
-	// Function to create one question in socket (for testing)
-	function clickQuestion(){
-		socket.emit("game-cycle", {
-			type:"question", 
-			payload:{questionID:"123456789", 
-			imageURL:"https://res.cloudinary.com/dat8yzztd/image/upload/v1747390409/soli7celoyncta2u4c4g.jpg", 
-			goodAnswer:["Toto", "Titi"], 
-			possibleAnswers:[["Toto", "Marc", "José"],["Titi", "Jean-Claude", "Chuck"]],
-			index:2,
-			askedAtTime:Date.now(),
-			answerHistory:[{playerID:"P1", answer:}]}})
-		navigation.navigate("Question")
-		// Handle question click
-		console.log("Question clicked");
-	}
+  // Function to create one question in socket (for testing)
+  function clickQuestion() {
+    // socket.emit("game-cycle", {
+    // 	type:"question",
+    // 	payload:{questionID:"123456789",
+    // 	imageURL:"https://res.cloudinary.com/dat8yzztd/image/upload/v1747390409/soli7celoyncta2u4c4g.jpg",
+    // 	goodAnswer:["Toto", "Titi"],
+    // 	possibleAnswers:[["Toto", "Marc", "José"],["Titi", "Jean-Claude", "Chuck"]],
+    // 	index:2,
+    // 	askedAtTime:Date.now(),
+    // 	answerHistory:[{playerID:"P1", answer:}]}})
+    // navigation.navigate("Question")
+    // // Handle question click
+    // console.log("Question clicked");
+  }
 
-	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>HOME (créer une partie --ADMIN-- ou rejoindre une partie -- joueur</Text>
-			<Button
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        HOME (créer une partie --ADMIN-- ou rejoindre une partie -- joueur
+      </Text>
+      <Button
         title="Start a game as Admin"
         onPress={() => navigation.navigate("HomeAdmin")}
       />
-			<Button title="Go to HomeTuto" onPress={() => navigation.navigate("HomeTuto")} />
-			<Button title="Go to PlayerName" onPress={() => navigation.navigate("PlayerName")} />
-			<Button title="Go to TabNavigator" onPress={() => navigation.navigate("TabNavigator")} />
-			<Button title="Go to QUESTION" onPress={() => clickQuestion()} />
-		</View>
-	);
+      <Button
+        title="Go to HomeTuto"
+        onPress={() => navigation.navigate("HomeTuto")}
+      />
+      <Button
+        title="Go to PlayerName"
+        onPress={() => navigation.navigate("PlayerName")}
+      />
+      <Button
+        title="Go to TabNavigator"
+        onPress={() => navigation.navigate("TabNavigator")}
+      />
+      <Button title="Go to QUESTION" onPress={() => clickQuestion()} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	// Add your styles here
-	// Example:
-	container: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
-	},
-	title: {
-		fontSize: 20,
-		fontWeight: "bold",
-		marginBottom: 20,
-	},
+  // Add your styles here
+  // Example:
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
 });

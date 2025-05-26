fecdd5af88ced5177484a95212f8405e41488089
pfbouquet <pfbouquet@gmail.com>
2025-05-26 13:36:38 +0200

    rename events

--- screens/GameLifeScreen.jsx
+++ screens/GameLifeScreen.jsx
@@ -9,38 +9,44 @@ export default function GameLifeScreen({ navigation }) {
   const socket = useContext(SocketContext);
   const game = useSelector((state) => state.game.value);
   const player = useSelector((state) => state.player.value);
-  const [phase, setPhase] = useState("preparation");
+  const [phase, setPhase] = useState("game-preparation");
 
   const dispatch = useDispatch();
 
-  let GameContent = <Text>Waiting for game to start...</Text>;
-
-  if (phase === "get-ready") {
-    GameContent = <GameLifeGetReadyForNextQuestion />;
-  } else if (phase === "question") {
-    GameContent = <Text>Next question</Text>;
-  }
-
   useEffect(() => {
     const handler = (data) => {
-      if (data.type === "next-question") {
-        setPhase("get-ready");
-      } else if (data.type === "go-next-question") {
-        dispatch(
-          newQuestion({
-            index: data.payload.index,
-            goodAnswers: data.payload.goodAnswers,
-            possibleAnswers: data.payload.possibleAnswers,
-            imageURL: data.payload.morphURL,
-          })
-        );
-        setPhase("question");
+      switch (data.type) {
+        case "next-question":
+          dispatch(
+            newQuestion({
+              index: data.payload.index,
+              goodAnswers: data.payload.goodAnswers,
+              possibleAnswers: data.payload.possibleAnswers,
+              imageURL: data.payload.morphURL,
+            })
+          );
+          setPhase("question-get-ready");
+          break;
+        case "go-question":
+          setPhase("question");
+          break;
+        case "go-scoreboard":
+          setPhase("scoreboard");
+          break;
       }
     };
 
     socket.on("game-cycle", handler);
     return () => socket.off("game-cycle", handler);
   }, []);
+
+  let GameContent = <Text>Waiting for game to start...</Text>;
+
+  if (phase === "question-get-ready") {
+    GameContent = <GameLifeGetReadyForNextQuestion />;
+  } else if (phase === "question") {
+    GameContent = <Text>Next question</Text>;
+  }
 
   return <View style={styles.container}>{GameContent}</View>;
 }

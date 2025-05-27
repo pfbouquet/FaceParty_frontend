f9432f578a3acd40601ff2c4f733372a7c2e0459
pfbouquet <pfbouquet@gmail.com>
2025-05-27 18:25:13 +0200

    kick player

---
+++ components/LobbyPlayerAdminMenu.js
@@ -0,0 +1,70 @@
+import { StyleSheet, View, TouchableOpacity, Button, Text } from "react-native";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+
+import { useEffect, useState } from "react";
+
+const EXPO_PUBLIC_BACKEND_URL = process.env.EXPO_PUBLIC_BACKEND_URL;
+
+export const LobbyPlayerAdminMenu = ({ playerID, roomID }) => {
+  useEffect(() => {
+    (async () => {
+      const result = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(result && result?.status === "granted");
+    })();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {/* Button to kick player, only visible to admin */}
+      <TouchableOpacity
+        style={styles.playerKickButton}
+        onPress={() => {
+          // TODO
+          fetch(`${EXPO_PUBLIC_BACKEND_URL}/games/kick-player`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              playerID: playerID,
+              roomID: roomID,
+            }),
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              if (!data.result) {
+                console.error(
+                  "Erreur: couldn't kick player out of the room.",
+                  data.error
+                );
+              }
+            })
+            .catch((error) => {
+              console.error("Erreur fetch:", error);
+              setLoading(false);
+            });
+        }}
+      >
+        <FontAwesome
+          name="remove"
+          size={40}
+          color="#de6b58"
+          style={styles.icon}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+  },
+  playerKickButton: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});

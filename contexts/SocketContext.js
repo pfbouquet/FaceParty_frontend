27e52b95746b4e4df932d4cd2ce167aced5e2ee8
pfbouquet <pfbouquet@gmail.com>
2025-05-29 20:14:59 +0200

    reset socket when HomeMulti is reached

--- contexts/SocketContext.js
+++ contexts/SocketContext.js
@@ -1,10 +1,13 @@
-import React, { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { io } from "socket.io-client";
+// load reducers
+import { useSelector } from "react-redux";
 
 export const SocketContext = createContext(null);
 const EXPO_PUBLIC_BACKEND_URL = process.env.EXPO_PUBLIC_BACKEND_URL;
 
 export const SocketProvider = ({ children }) => {
+  const gameID = useSelector((state) => state.game.value.gameID);
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
@@ -21,9 +24,10 @@ export const SocketProvider = ({ children }) => {
     });
 
     return () => {
+      console.log("🛑 Disconnecting socket:", socket?.id);
       newSocket.disconnect();
     };
-  }, []);
+  }, [EXPO_PUBLIC_BACKEND_URL, gameID]);
 
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
